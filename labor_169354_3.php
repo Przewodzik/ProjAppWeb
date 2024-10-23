35938a97cb383355690ebe124d0b37d15181000c
Przewodzik <lukasz_prze@wp.pl>
2024-10-23 13:14:19 +0200

    Create labor_169354_3.php

---
+++ labor_169354_3.php
@@ -0,0 +1,6 @@
+<?php
+    $nr_indeksu = '169354';
+    $nrGrupy = '3';
+    echo 'Łukasz Przewodowski ' . $nr_indeksu . ' grupa ' . $nrGrupy . ' <br/><br/>';
+    echo 'Zastosowanie metody include() <br/>';
+?>
