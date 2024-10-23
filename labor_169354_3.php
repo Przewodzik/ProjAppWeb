e8b2870a7472fb7b82af428d95c315090909fb92
Przewodzik <lukasz_prze@wp.pl>
2024-10-23 13:16:41 +0200

    delete

--- labor_169354_3.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $nr_indeksu = '169354';
-    $nrGrupy = '3';
-    echo 'Łukasz Przewodowski ' . $nr_indeksu . ' grupa ' . $nrGrupy . ' <br/><br/>';
-    echo 'Zastosowanie metody include() <br/>';
-?>
